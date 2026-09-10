6a9c85711e7e5a6fb504bdb3bfa665c5200ad11c
ACSClaw <242596880+ACSClaw@users.noreply.github.com>
2026-09-10 11:09:43 +0200

    Affichage Addition fonctionnelle...

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -4,5 +4,6 @@ public class main{
 
     public static void main(String[] args){
         System.out.println("Calculatrice");
+        System.out.println("Addition fonctionnelle dans la calculatrice");
     }
 }
