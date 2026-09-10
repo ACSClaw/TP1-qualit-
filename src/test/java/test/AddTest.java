70982f39de379ee3f9606d88f1ed9af17abda60f
ACSClaw <242596880+ACSClaw@users.noreply.github.com>
2026-09-10 10:54:18 +0200

    Ajout Commentaire et test

--- src/test/java/test/AddTest.java
+++ src/test/java/test/AddTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AddTest {
 
+    //Test qui vérifie le cas positif
     @Test
     public void testAddition() {
         Add add = new Add();
@@ -14,6 +15,15 @@ class AddTest {
         int resultat = add.add(1, 2);
 
         assertEquals(3, resultat);
+    }
+
+    //Test qui vérifie les cas négatifs
+    @Test
+    public void testAdditionNegatif(){
+
+        Add addNegatif = new Add();
+        int resultatNegatif = addNegatif.add(2, 3);
+        assertEquals(-1, resultatNegatif);
     }
 
 }
